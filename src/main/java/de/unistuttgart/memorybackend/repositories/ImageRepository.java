76a6767239a6689098a06b55aeb52d0c4b26fedc
Fabian <fabian.kalitzke@arcor.de>
2025-01-06 18:18:22 +0100

    Update ImageRepository.java

--- src/main/java/de/unistuttgart/memorybackend/repositories/ImageRepository.java
+++ src/main/java/de/unistuttgart/memorybackend/repositories/ImageRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, UUID> {
-    Optional<Image> findByImageUUID(UUID imageUUID);
+    Optional<Image> findByImageUUID(UUID uuid);
 }
